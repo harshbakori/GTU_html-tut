27cad2599d868e1aff2b0dfec086181b0778f800
HARSH <bakoriharsh@gmail.com>
2018-12-23 14:09:31 +0530

    the first commit

---
+++ the html all test/graph_line_use.ob.js
@@ -0,0 +1,34 @@
+var _$_85d1 = ["top", "center", "bold", "calibri", "dimGrey", "line", "Float", "f(x)", "Chart", "render"];
+
+function graphline(_0xD9B9, _0xD9A9, _0xD9C9, _0xD9D9) {
+    var _0xD999 = new CanvasJS[_$_85d1[8]](_0xD9B9, {
+        zoomEnabled: false,
+        toolTip: {
+            shared: false
+        },
+        legend: {
+            verticalAlign: _$_85d1[0],
+            horizontalAlign: _$_85d1[1],
+            fontSize: 1,
+            fontWeight: _$_85d1[2],
+            fontFamily: _$_85d1[3],
+            fontColor: _$_85d1[4]
+        },
+        axisY: {
+            title: _0xD9D9,
+            includeZero: false
+        },
+        axisX: {
+            title: _0xD9C9
+        },
+        data: [{
+            type: _$_85d1[5],
+            xValueType: _$_85d1[6],
+            showInLegend: false,
+            name: _$_85d1[7],
+            markerSize: 10,
+            dataPoints: _0xD9A9
+        }]
+    });
+    _0xD999[_$_85d1[9]]()
+}
